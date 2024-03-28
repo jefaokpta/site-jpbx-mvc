39a4ea756e8e19aab32257bc4b03e15f4e744dcc
jefaokpta <jefaokpta@hotmail.com>
2024-03-28 17:33:37 -0300

    menu nao percebe scroll e nao muda fundo

---
+++ src/main/kotlin/br/com/jpbx/sitejpbx/SiteJpbxApplication.kt
@@ -0,0 +1,11 @@
+package br.com.jpbx.sitejpbx
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SiteJpbxApplication
+
+fun main(args: Array<String>) {
+    runApplication<SiteJpbxApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/jpbx/sitejpbx/SiteJpbxApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.jpbx.sitejpbx
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SiteJpbxApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
