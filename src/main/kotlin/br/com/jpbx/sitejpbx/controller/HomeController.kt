6ba550d4a4199691bb7e04179c009c5399756c9c
jefaokpta <jefaokpta@hotmail.com>
2024-03-31 11:56:35 -0300

    falta implemetar lib de email

--- src/main/kotlin/br/com/jpbx/sitejpbx/controller/HomeController.kt
+++ src/main/kotlin/br/com/jpbx/sitejpbx/controller/HomeController.kt
@@ -1,12 +1,39 @@
 package br.com.jpbx.sitejpbx.controller
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.*
 
 @Controller
+@RequestMapping("/")
 class HomeController {
 
-    @GetMapping("/")
+    @GetMapping
     fun home() = "home"
 
+}
+
+@RestController
+@RequestMapping("/api")
+class MessageController {
+
+    @PostMapping("/new-message")
+    fun newMessage(
+        @RequestParam("name") name: String,
+        @RequestParam("email") email: String,
+        @RequestParam("message") message: String,
+        @RequestHeader(HttpHeaders.HOST) host: String
+    ): String {
+        println("Name: $name")
+        println("Email: $email")
+        println("Message: $message")
+        println("Host: $host")
+
+        if (host != "localhost:8080") {
+            throw RuntimeException("Host invalido!")
+        }
+
+        return jacksonObjectMapper().writeValueAsString(mapOf("response" to "Message received!"))
+    }
 }
