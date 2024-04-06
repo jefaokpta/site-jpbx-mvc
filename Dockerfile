a3b9c0681089fbe070a3065f62a76852d1b2bf0a
jefaokpta <jefaokpta@hotmail.com>
2024-04-06 12:54:58 -0300

    ok pra deploy DO

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM eclipse-temurin:21
 COPY ./target/*.jar app.jar
-ENTRYPOINT ["java", "-Xms192m", "-Xmx192m", "-XX:+UseSerialGC", "-jar" ,"/app.jar"]
+ENTRYPOINT ["java", "-Xms384m", "-Xmx384m", "-XX:+UseSerialGC", "-XX:ActiveProcessorCount=1", "-jar" ,"/app.jar"]
 # NAO ESQUCA DE BUILDAR NOVO JAR
--- src/main/kotlin/br/com/jpbx/sitejpbx/bot/TelegramInit.kt
+++ src/main/kotlin/br/com/jpbx/sitejpbx/bot/TelegramInit.kt
@@ -2,16 +2,20 @@ package br.com.jpbx.sitejpbx.bot
 
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.event.EventListener
+import org.springframework.scheduling.annotation.Async
+import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.stereotype.Component
 import org.telegram.telegrambots.longpolling.TelegramBotsLongPollingApplication
 
 @Component
+@EnableAsync
 class TelegramInit(
     private val telegramSecurity: TelegramSecurity,
     private val telegramBot: TelegramBot
 ) {
 
     @EventListener(ApplicationReadyEvent::class)
+    @Async
     fun run(){
         val telegramBotsLongPollingApplication = TelegramBotsLongPollingApplication()
         telegramBotsLongPollingApplication.registerBot(telegramSecurity.getTelegramBotToken(), telegramBot)
