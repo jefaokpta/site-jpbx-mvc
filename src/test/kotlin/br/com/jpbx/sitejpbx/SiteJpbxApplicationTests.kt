e8b05dadc49c3220b818e72801b6c5ce4bdc7970
jefaokpta <jefaokpta@hotmail.com>
2024-05-04 11:08:46 -0300

    testes melhorados

--- src/test/kotlin/br/com/jpbx/sitejpbx/SiteJpbxApplicationTests.kt
+++ src/test/kotlin/br/com/jpbx/sitejpbx/SiteJpbxApplicationTests.kt
@@ -1,10 +1,10 @@
 package br.com.jpbx.sitejpbx
 
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
-@Disabled
+@ActiveProfiles("test")
 @SpringBootTest
 class SiteJpbxApplicationTests {
 
